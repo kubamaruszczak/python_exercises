19bd83a46cae63655f1b4adf4b3cfcf771e8adf4
kubamaruszczak <kubamaruszczak@gmail.com>
2022-11-30 09:36:26 +0100

    Will it rain today func added

--- rain_alert_sms/main.py
+++ rain_alert_sms/main.py
@@ -1,17 +1,28 @@
 import requests
 
-MY_LAT = 50.064651
-MY_LONG = 19.944981
-API_KEY = "YOUR_API_KEY"
 
-parameters = {
-    "lat": MY_LAT,
-    "lon": MY_LONG,
-    "appid": API_KEY,
-    "units": "metric",
-}
+def will_rain_today():
+    """Returns True if it will rain in next 12 hours and False otherwise"""
+    api_key = "YOUR_API_KEY"
+    opw_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+    weather_params = {
+        "lat": 50.064651,
+        "lon": 19.944981,
+        "appid": api_key,
+    }
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
-response.raise_for_status()
-data = response.json()
-print(data["list"][:8])  # one day weather with every 3 hour stamp
+    response = requests.get(url=opw_endpoint, params=weather_params)
+    response.raise_for_status()
+    weather_data = response.json()
+    today_weather = weather_data["list"][:5]  # weather for the next 12 hours
+    for condition in today_weather:
+        weather_code = condition["weather"][0]["id"]
+        if weather_code < 600:
+            return True
+    return False
+
+
+if will_rain_today():
+    print("You will need an umbrella.")
+else:
+    print("You are fine.")
