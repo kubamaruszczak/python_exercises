e168fb367d741cba89dae050792c129f79a21bee
kubamaruszczak <kubamaruszczak@gmail.com>
2022-11-30 11:28:50 +0100

    Notification via sms using Twilio added

--- rain_alert_sms/main.py
+++ rain_alert_sms/main.py
@@ -1,9 +1,13 @@
 import requests
+from twilio.rest import Client
+
+TWILIO_NUM = "+123123123"
+MY_NUM = "+321321321"
 
 
-def will_rain_today():
+def will_rain_today() -> bool:
     """Returns True if it will rain in next 12 hours and False otherwise"""
-    api_key = "YOUR_API_KEY"
+    api_key = "your_api_key"
     opw_endpoint = "https://api.openweathermap.org/data/2.5/forecast"
     weather_params = {
         "lat": 50.064651,
@@ -16,13 +20,21 @@ def will_rain_today():
     weather_data = response.json()
     today_weather = weather_data["list"][:5]  # weather for the next 12 hours
     for condition in today_weather:
-        weather_code = condition["weather"][0]["id"]
+        weather_code = int(condition["weather"][0]["id"])
         if weather_code < 600:
             return True
     return False
 
 
+def send_sms():
+    account_sid = "your_acc_sid"
+    auth_token = "your_auth_token"
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(body="It's going to rain today. Remember to bring an umbrella! ☔",
+                                     from_=TWILIO_NUM,
+                                     to=MY_NUM)
+    print(message.status)
+
+
 if will_rain_today():
-    print("You will need an umbrella.")
-else:
-    print("You are fine.")
+    send_sms()
