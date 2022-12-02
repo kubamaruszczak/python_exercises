f99c85a47d88d178c2b1500d444628b6ce6f26ab
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-02 09:26:52 +0100

    New graph creation. Using header with POST

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,14 +1,36 @@
 import requests
 import os
 
-pixela_endpoint = "https://pixe.la/v1/users"
-user_params = {
-    "token": os.environ.get("PIXELA_TOKEN"),
-    "username": "mariuszmtwc",
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
+USERNAME = "mariuszmtwc"
+TOKEN = os.environ.get("PIXELA_TOKEN")
+
+pixela_url = "https://pixe.la"
+
+# Create a pixela account
+# user_params = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes",
+# }
+#
+# create_user_endpoint = "/v1/users"
+# response = requests.post(url=f"{pixela_url}{create_user_endpoint}", json=user_params)
+# print(response.text)
+
+# Set up a graph
+graph_config = {
+    "id": "graph1",
+    "name": "Python Learning",
+    "unit": "minutes",
+    "type": "int",
+    "color": "shibafu",  # green
 }
 
-# Set up a pixela account
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+graph_endpoint = f"/v1/users/{USERNAME}/graphs"
+response = requests.post(url=f"{pixela_url}{graph_endpoint}", json=graph_config, headers=headers)
+print(response.text)
