0559ff766e623d96dcb7396f91f88077948c9e57
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-02 15:05:17 +0100

    Update pixel fun added

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -8,41 +8,67 @@ GRAPH_ID = "graph1"
 
 pixela_url = "https://pixe.la"
 
-# Create a pixela account
-# user_params = {
-#     "token": TOKEN,
-#     "username": USERNAME,
-#     "agreeTermsOfService": "yes",
-#     "notMinor": "yes",
-# }
-#
-# create_user_endpoint = "/v1/users"
-# response = requests.post(url=f"{pixela_url}{create_user_endpoint}", json=user_params)
-# print(response.text)
 
-# Set up a graph
-graph_config = {
-    "id": GRAPH_ID,
-    "name": "Python Learning",
-    "unit": "minutes",
-    "type": "int",
-    "color": "shibafu",  # green
-}
+def create_account(username: str, token: str):
+    """Create pixela account for given username and token"""
+    user_params = {
+        "token": token,
+        "username": username,
+        "agreeTermsOfService": "yes",
+        "notMinor": "yes",
+    }
 
-headers = {
-    "X-USER-TOKEN": TOKEN,
-}
+    create_user_endpoint = "/v1/users"
+    response = requests.post(url=f"{pixela_url}{create_user_endpoint}", json=user_params)
+    print(response.text)
 
-# graph_endpoint = f"/v1/users/{USERNAME}/graphs"
-# response = requests.post(url=f"{pixela_url}{graph_endpoint}", json=graph_config, headers=headers)
-# print(response.text)
 
-# Post a pixel
-pixel_params = {
-    "date": datetime.now().strftime("%Y%m%d"),
-    "quantity": "120",
-}
+def create_graph(username: str, graph_id: str, name: str, unit: str, value_type: str, color: str):
+    """Creates piexla graph with given parameters"""
+    graph_config = {
+        "id": graph_id,
+        "name": name,
+        "unit": unit,
+        "type": value_type,
+        "color": color,  # green
+    }
+    headers = {
+        "X-USER-TOKEN": TOKEN,
+    }
+    graph_endpoint = f"/v1/users/{username}/graphs"
+    response = requests.post(url=f"{pixela_url}{graph_endpoint}", json=graph_config, headers=headers)
+    print(response.text)
 
-post_pixel_endpoint = f"/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
-response = requests.post(url=f"{pixela_url}{post_pixel_endpoint}", json=pixel_params, headers=headers)
-print(response.text)
+
+def post_pixel(username: str, graph_id: str, date: str, quantity: str):
+    """Posts a pixel for a given date and quantity"""
+    pixel_params = {
+        "date": date,
+        "quantity": quantity,
+    }
+    headers = {
+        "X-USER-TOKEN": TOKEN,
+    }
+    post_pixel_endpoint = f"/v1/users/{username}/graphs/{graph_id}"
+    response = requests.post(url=f"{pixela_url}{post_pixel_endpoint}", json=pixel_params, headers=headers)
+    print(response.text)
+
+
+def update_pixel(username: str, graph_id: str, date: str, new_quantity: str):
+    new_pixel_params = {
+        "quantity": new_quantity
+    }
+    headers = {
+        "X-USER-TOKEN": TOKEN,
+    }
+    update_pixel_endpoint = f"/v1/users/{username}/graphs/{graph_id}/{date}"
+    print(update_pixel_endpoint)
+    response = requests.put(url=f"{pixela_url}{update_pixel_endpoint}", json=new_pixel_params, headers=headers)
+    print(response.text)
+
+
+# create_account(USERNAME, TOKEN)
+# create_graph(USERNAME, GRAPH_ID, "Python Learning", "minutes", "int", "shibafu")
+today = datetime.now().strftime("%Y%m%d")
+# post_pixel(USERNAME, GRAPH_ID, today, "90")
+update_pixel(USERNAME, GRAPH_ID, today, "120")
