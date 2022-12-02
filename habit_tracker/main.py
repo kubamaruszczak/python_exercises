9bf8c02c8908bb99c4ee4d6087763fcf40921a64
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-02 14:25:47 +0100

    Add pixel functionality

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,8 +1,10 @@
 import requests
 import os
+from datetime import datetime
 
 USERNAME = "mariuszmtwc"
 TOKEN = os.environ.get("PIXELA_TOKEN")
+GRAPH_ID = "graph1"
 
 pixela_url = "https://pixe.la"
 
@@ -20,7 +22,7 @@ pixela_url = "https://pixe.la"
 
 # Set up a graph
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "Python Learning",
     "unit": "minutes",
     "type": "int",
@@ -31,6 +33,16 @@ headers = {
     "X-USER-TOKEN": TOKEN,
 }
 
-graph_endpoint = f"/v1/users/{USERNAME}/graphs"
-response = requests.post(url=f"{pixela_url}{graph_endpoint}", json=graph_config, headers=headers)
+# graph_endpoint = f"/v1/users/{USERNAME}/graphs"
+# response = requests.post(url=f"{pixela_url}{graph_endpoint}", json=graph_config, headers=headers)
+# print(response.text)
+
+# Post a pixel
+pixel_params = {
+    "date": datetime.now().strftime("%Y%m%d"),
+    "quantity": "120",
+}
+
+post_pixel_endpoint = f"/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+response = requests.post(url=f"{pixela_url}{post_pixel_endpoint}", json=pixel_params, headers=headers)
 print(response.text)
