b0631cc9ea3eeb13c15d6324e60d887730241add
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-12 19:25:44 +0100

    spotify auth

--- musical_time_machine/main.py
+++ musical_time_machine/main.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 billboard_url = "https://www.billboard.com/charts/hot-100"
 
@@ -15,5 +18,10 @@ li_tags = soup.find_all(name="li", class_="o-chart-results-list__item")
 titles = [tag.find(name="h3", class_="c-title").getText().strip() for tag in li_tags
           if tag.find(name="h3", class_="c-title") is not None]
 
+# Spotify authentication
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-library-read"))
 
-
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
