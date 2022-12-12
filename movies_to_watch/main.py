d13a57d586f321c21c4cf7ab268a04ca2aa014b2
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-12 13:28:43 +0100

    scraping movie titles and writing to file

--- movies_to_watch/main.py
+++ movies_to_watch/main.py
@@ -6,3 +6,8 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 response = requests.get(url=URL)
 empire_webpage = response.text
 soup = BeautifulSoup(empire_webpage, "html.parser")
+movies = [tag.getText() for tag in soup.find_all(name="h3", class_="title")]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies[::-1]:
+        file.write(f"{movie}\n")
