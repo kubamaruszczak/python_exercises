7b2bf7aa8e19d7ed0d06a957dbeead57f1f1366a
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-13 09:39:15 +0100

    sending email when the price in lower than threshold

--- amazon_price_tracker/main.py
+++ amazon_price_tracker/main.py
@@ -1,6 +1,10 @@
 import os
 from requests import get
 from bs4 import BeautifulSoup
+from smtplib import SMTP
+
+price_threshold = 120  # $
+product_name = "White FOX MTB Helmet"
 
 # Get product data from Amazon
 product_url = "https://www.amazon.com/RACING-Youth-Rampage-Mountain-Helmet/dp/B08SG9HQXN/ref=cs_sr_dp?crid=3E3FS83PZQBHD&keywords=fox%2Bhelmet%2Bwhite&qid=1670917617&sprefix=fox%2Bhelmet%2Bwhi%2Caps%2C165&sr=8-2&th=1&psc=1"
@@ -17,4 +21,18 @@ soup = BeautifulSoup(amazon_contents, "html.parser")
 price_decimal = soup.find(name="span", class_="a-price-whole").getText()
 price_fraction = soup.find(name="span", class_="a-price-fraction").getText()
 price = float(price_decimal + price_fraction)
+
+# Send me an email when the product price is below defined threshold
+if price < price_threshold:
+    with SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        my_email = os.environ.get("EMAIL")
+        connection.login(user=my_email,
+                         password=os.environ.get("PASSWORD"))
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=my_email,
+                            msg=f"Subject:{product_name} price alert!\n\n"
+                                f"{product_name} price is now lower than {price_threshold} $\n"
+                                f"{product_url}")
+
 
