4a71e1b8edb6296cf1b0d9b81b9b480238452edd
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-17 11:51:32 +0100

    auto liking

--- auto_tinder_swiping_bot/main.py
+++ auto_tinder_swiping_bot/main.py
@@ -7,10 +7,12 @@ import os
 driver = webdriver.Chrome()
 driver.get("https://tinder.com/")
 
+# Agree to cookies
 sleep(1)
 agree_cookies = driver.find_element("xpath", "//*[@id='q888578821']/div/div[2]/div/div/div[1]/div[1]/button/div[2]/div[2]")
 agree_cookies.click()
 
+# Log in via facebook
 sleep(1)
 log_in = driver.find_element("xpath", "//*[@id='q888578821']/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]")
 log_in.click()
@@ -21,6 +23,7 @@ via_fb.click()
 
 sleep(1)
 windows = driver.window_handles
+tinder_window = windows[0]
 fb_window = windows[1]
 driver.switch_to.window(fb_window)
 sleep(1)
@@ -30,7 +33,29 @@ email_input = driver.find_element("name", "email")
 email_input.send_keys(os.environ.get("EMAIL"))
 password_input = driver.find_element("name", "pass")
 password_input.send_keys(os.environ.get("PASSWORD"))
+sleep(2)
 password_input.send_keys(Keys.ENTER)
 
-sleep(600)
-driver.close()
+# Agree to location pop up
+driver.switch_to.window(windows[0])
+sleep(5)
+location_button = driver.find_element("xpath", '//*[@id="q-839802255"]/main/div/div/div/div[3]/button[1]/div[2]/div[2]')
+location_button.click()
+
+# Disagree to notification pop up
+sleep(5)
+notifications_button = driver.find_element("xpath", '//*[@id="q-839802255"]/main/div/div/div/div[3]/button[2]/div[2]/div[2]')
+notifications_button.click()
+
+# Close pop up with dark mode
+sleep(2)
+dark_mode_close = driver.find_element("xpath", '//*[@id="q-839802255"]/main/div/div[2]/button/svg/path')
+dark_mode_close.click()
+
+while True:
+    sleep(1.5)
+    like_button = driver.find_element("xpath", '//*[@id="q888578821"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div/div[4]/button/span/span/svg/path')
+    like_button.click()
+
+# sleep(600)
+# driver.close()
