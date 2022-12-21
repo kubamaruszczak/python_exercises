c36151d04584719158b598792e393c393f2fd197
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-21 11:05:38 +0100

    higher lower game web version

--- higher_lower_flask/server.py
+++ higher_lower_flask/server.py
@@ -1,4 +1,8 @@
 from flask import Flask
+from random import randint
+
+num_to_guess = randint(0, 9)
+print(num_to_guess)
 
 app = Flask(__name__)
 
@@ -7,6 +11,20 @@ app = Flask(__name__)
 def main_page():
     return '<h1>Guess a number between 0 and 9</h1>' \
            '<img src="https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif">'
+
+
+@app.route('/<int:number>')
+def guess_page(number):
+    if number == num_to_guess:
+        return '<h1 style="color: green;">You found me!</h1>' \
+               '<img src="https://media3.giphy.com/media/BPJmthQ3YRwD6QqcVD/giphy.gif?cid=ecf05e47ayvqrc7n7hofio87f' \
+               'q6qs69000eszx7y6p0gakk1&rid=giphy.gif&ct=g">'
+    elif number > num_to_guess:
+        return '<h1 style="color: purple;">Too high, try again!</h1>' \
+               '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
+    else:
+        return '<h1 style="color: red;">Too low, try again!</h1>' \
+               '<img src="https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
 
 
 if __name__ == "__main__":
