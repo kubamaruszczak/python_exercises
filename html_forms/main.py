00756902e5aa4e97d94df8eb5a7f207ecdc6f9ff
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-24 22:05:40 +0100

    getting data from html form with flask

--- html_forms/main.py
+++ html_forms/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/form-entry', methods=["POST"])
+def receive_data():
+    username = request.form['username']
+    password = request.form['password']
+    return render_template("login.html", u_name=username, u_pass=password)
 
 
 if __name__ == "__main__":
