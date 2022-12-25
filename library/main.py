f865924be26c9f0a63a2af051927c29038c71c9d
kubamaruszczak <kubamaruszczak@gmail.com>
2022-12-25 23:11:43 +0100

    database with books info integration

--- library/main.py
+++ library/main.py
@@ -1,24 +1,40 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
+# Create a books database
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+db.init_app(app)
 
-all_books = []
+
+# Create a table
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Book).all()
     return render_template("index.html", books=all_books)
 
 
 @app.route("/add", methods=["POST", "GET"])
 def add():
     if request.method == "POST":
-        book = {
-            "title": request.form['title'],
-            "author": request.form['author'],
-            "rating": request.form['rating'],
-        }
-        all_books.append(book)
+        new_book = Book(title=request.form['title'],
+                        author=request.form['author'],
+                        rating=request.form['rating'])
+        # Add new book to the database
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for("home"))
 
     return render_template("add.html")
